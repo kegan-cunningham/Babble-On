00d80ee33edd2f3dc196e42b9cb0a18d32d0c7d5
Kegan Cunningham <kcunningham04@hotmail.com>
2018-04-26 19:31:48 -0400

    Add frontend structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:new, :create, :edit, :update, :show, :index]
+  end
+
+  root 'static_pages#root'
 end
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/babble-on.jsx",
+  entry: "./frontend/babblio.jsx",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
