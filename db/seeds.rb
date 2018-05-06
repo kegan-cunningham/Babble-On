8cdb2c3936ebdb79d2c5eba2a55c45098f9d793f
Kegan Cunningham <kcunningham04@hotmail.com>
2018-05-06 10:34:57 -0400

    Adjust styling and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,40 @@ u1 = User.new(
     firstname: 'Kegan',
     lastname: 'Cunningham',
     password: 'asdfasdf',
-    bio: 'believe me'
+    bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+          and more recently with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum.'
   )
   u1.save!
+u2 = User.new(
+    username: 'asai2',
+    firstname: 'Kegan2',
+    lastname: 'Cunningham2',
+    password: 'asdfasdf2',
+    bio: '222222222222222222222222222222222222222222222222222222222222222222222222222222222
+    222222222222222222222222222222222222222222222222222222222222222222222222222222222
+    222222222222222222222222222222222222222222222222222222222222222222222222222222222
+    222222222222222222222222222222222222222222222222222222222222222222222222222222222'
+  )
+  u2.save!
+u3 = User.new(
+    username: 'asai3',
+    firstname: 'Kegan3',
+    lastname: 'Cunningham3',
+    password: 'asdfasdf3',
+    bio: '333333333333333333333 3 33 3 333 333 333333 33 33 33 3 33333'
+  )
+  u3.save!
+u4 = User.new(
+    username: 'asai4',
+    firstname: 'Kegan4',
+    lastname: 'Cunningham4',
+    password: 'asdfasdf4',
+    bio: 'believe me4'
+  )
+  u4.save!
